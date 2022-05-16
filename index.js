5432dcf2f4f834ccc0ffa791f54575830b81e064
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-16 18:34:37 +0100

    commit tree e38b32c011bffc161dc0b21fa82d5e0bec83cb3f

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(requestIdleCallback){
+  let greeting;
+  if (requestIdleCallback<400){
+    greeting = "This one is on me!";
+  }
+  else if (requestIdleCallback>=400, requestIdleCallback<2000){
+    greeting = 'That will be twenty bucks.';
+  }
+  else if (requestIdleCallback>= 2000, requestIdleCallback<2500){
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else if (requestIdleCallback>= 2500){
+    greeting = 'No can do.'
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? "Ok, sounds good.": "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch(tip){
+      case 'generous':
+        return 'Thank you so much.'
+          break
+      case 'not as generous':
+        return 'Thank you.'
+          break
+      default:
+        return 'Bye.'
+    }
+  
 }
